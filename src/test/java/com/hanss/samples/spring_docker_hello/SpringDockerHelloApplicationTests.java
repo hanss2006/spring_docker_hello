c796dfdcaebb52a3a9143df6c42d3b226bf880ae
pgrudanov <hanss@list.ru>
2024-01-24 15:13:14 +0300

    initial commit

---
+++ src/test/java/com/hanss/samples/spring_docker_hello/SpringDockerHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanss.samples.spring_docker_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
