ac56642324b9e3b0850d1dbb8bf0f5276b90697f
pgrudanov <hanss@list.ru>
2024-01-24 15:37:55 +0300

    initial commit

--- src/main/java/com/hanss/samples/spring_docker_hello/SpringDockerHelloApplication.java
+++ src/main/java/com/hanss/samples/spring_docker_hello/SpringDockerHelloApplication.java
@@ -2,10 +2,16 @@ package com.hanss.samples.spring_docker_hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringDockerHelloApplication {
-
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringDockerHelloApplication.class, args);
     }
